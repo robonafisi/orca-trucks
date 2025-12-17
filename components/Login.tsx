b34c9aed1a3086ffe8a06afbc7b36962ca1e1dec
Mohammad Nafisi <nafisidev@gmail.com>
2025-12-16 22:05:17 -0800

    New

--- components/Login.tsx
+++ components/Login.tsx
@@ -54,7 +54,6 @@ export const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
               onChange={(e) => setUsername(e.target.value)}
               required
               className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="fleetmanager"
             />
           </div>
 
